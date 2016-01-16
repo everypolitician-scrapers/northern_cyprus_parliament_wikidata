3e74771c847a89e1440c5fa8b3b144c044eb19e6
Tony Bowden <tony@tmtm.com>
2016-01-16 09:41:04 +0000

    Modernise scraper

--- scraper.rb
+++ scraper.rb
@@ -1,30 +1,9 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'json'
-require 'pry'
-require 'rest-client'
-require 'scraperwiki'
 require 'wikidata/fetcher'
-require 'mediawiki_api'
 
-def members
-  morph_api_url = 'https://api.morph.io/tmtmtmtm/northern_cyprus_parliament_wikipedia/data.json'
-  morph_api_key = ENV["MORPH_API_KEY"]
-  result = RestClient.get morph_api_url, params: {
-    key: morph_api_key,
-    query: "select DISTINCT(wikiname) AS wikiname from data"
-  }
-  JSON.parse(result, symbolize_names: true)
-end
+names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/northern_cyprus_parliament_wikipedia', column: 'wikiname')
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: names })
+warn EveryPolitician::Wikidata.notify_rebuilder
 
-WikiData.ids_from_pages('en', members.map { |c| c[:wikiname] }).each_with_index do |p, i|
-  data = WikiData::Fetcher.new(id: p.last).data('tr') rescue nil
-  unless data
-    warn "No data for #{p}"
-    next
-  end
-  ScraperWiki.save_sqlite([:id], data)
-end
-
-warn RestClient.post ENV['MORPH_REBUILDER_URL'], {} if ENV['MORPH_REBUILDER_URL']
