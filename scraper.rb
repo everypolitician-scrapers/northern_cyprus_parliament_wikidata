4df66edc3a8bdf8ec14abdbc81d241b159aa5280
Tony Bowden <tony@tmtm.com>
2019-01-22 16:00:29 +0000

    Also get data from Turkish Wikipedia

--- scraper.rb
+++ scraper.rb
@@ -4,4 +4,10 @@
 require 'wikidata/fetcher'
 
 names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/northern_cyprus_parliament_wikipedia', column: 'wikiname')
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: names })
+
+tr_names = EveryPolitician::Wikidata.wikipedia_xpath(
+  url: 'https://tr.wikipedia.org/wiki/KKTC_Cumhuriyet_Meclisi_8._d%C3%B6nem_milletvekilleri_listesi',
+  xpath: '//table[.//th[contains(.,"Milletvekili")]]//td//a[not(@class="new")]/@title',
+)
+
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: names, tr: tr_names })
